51e5d0c9f2434712f0d7cf4e2a66a1f9112858dc
r44v <rs.raav@gmail.com>
2020-12-02 17:57:16 +0100

    feat: day 2

---
+++ 2/part_two.py
@@ -0,0 +1,20 @@
+import pathlib
+
+# Get current file location
+file_location = pathlib.Path(__file__).parent
+
+# Get input data
+p = pathlib.Path(file_location, 'input.txt')
+with p.open('r', encoding='utf-8') as fs:
+    input_list = [x.strip() for x in fs.readlines()]
+
+
+def check_if_valid(line: str) -> bool:
+    condition, password = str.split(line, ': ')
+    condition, character = str.split(condition, ' ')
+    condition_a, condition_b = str.split(condition, '-')
+    return (password[int(condition_a)-1] == character) ^ (password[int(condition_b)-1] == character)
+
+
+total_matching_passwords = sum([1 for x in input_list if check_if_valid(x)])
+print(total_matching_passwords)
